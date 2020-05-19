2310a1df4a5aa385550882dc8cb4052bdc6235b3
Nick Woods <nick.woods.wi@gmail.com>
2020-05-19 17:28:22 -0500

    committing for deployment

--- postgrator-config.js
+++ postgrator-config.js
@@ -3,7 +3,7 @@ module.exports = {
     "migrationsDirectory": "migrations",
     "driver": "pg",
     "connectionString": (process.env.NODE_ENV === 'test')
-        ? process.env.DB_URL
-        : process.env.DB_URL,
+        ? process.env.DATABASE_URL
+        : process.env.DATABASE_URL,
     "ssl": !!process.env.SSL,
 }
